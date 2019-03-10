eb5cb173c1b4ff0bbdca1124a5dfd5ac23dbc3e7
aamirpinger <aamirpinger@yahoo.com>
2019-03-10 14:08:47 +0500

    Simple View (container) example

---
+++ React-Native-Features-Components/ViewComponentExample.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class ViewComponentExample extends Component {
+    render() {
+        return (
+            // Try setting `flexDirection` to `column`.
+            <View style={{
+                flexDirection: 'row', height: 100, padding: 20,
+                backgroundColor: 'black'
+            }}>
+                <View style={{ backgroundColor: 'white', flex: 2 }} />
+                <View style={{ backgroundColor: 'green', flex: 5 }} />
+            </View>
+        );
+    }
+
+};
+
