503320096dfe13112068ac400bdfa1a98561080a
aamirpinger <aamirpinger@yahoo.com>
2019-03-10 13:27:54 +0500

    Basic Style component example

--- React-Native-Features-Components/StyleComponent.js
+++ React-Native-Features-Components/StyleComponent.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
 const styles = StyleSheet.create({
   bigBlue: {
