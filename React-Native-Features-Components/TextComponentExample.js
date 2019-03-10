6056caf56f660d2b24e3be19c0f6ad97c4abb0df
aamirpinger <aamirpinger@yahoo.com>
2019-03-10 14:08:14 +0500

    Simple Text rendering Example

---
+++ React-Native-Features-Components/TextComponentExample.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    StyleSheet
+} from 'react-native'
+
+export default class TextComponentExample extends Component {
+    render() {
+        return (
+            <Text numberOfLines={2} style={{ fontFamily: 'Roboto' }}>
+                <Text style={styles.titleText} onPress={this.onPressTitle}>
+                    Bird's Nest{'\n'}
+                </Text>
+                <Text>
+                    This is not really a bird nest.This is not really a bird nest.
+                    This is not really a bird nest.This is not really a bird nest.
+                    This is not really a bird nest.This is not really a bird nest.
+                </Text>
+            </Text>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    titleText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+});
+
